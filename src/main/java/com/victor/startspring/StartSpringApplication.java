71de1298b231ec5407bac6105bebdc00e2d9899c
Victor <josevictomobile@gmail.com>
2018-08-30 21:20:16 -0300

    spring initializr

---
+++ src/main/java/com/victor/startspring/StartSpringApplication.java
@@ -0,0 +1,12 @@
+package com.victor.startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/victor/startspring/StartSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.victor.startspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
